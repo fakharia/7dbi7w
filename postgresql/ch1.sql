c7e5e1752428e346b55206478910c1b2453ddf8c
Kerry Buckley <kerryjbuckley@gmail.com>
2012-10-04 13:10:19 +0100

    Add mini-exercises from within chapter 1

--- postgresql/ch1.sql
+++ postgresql/ch1.sql
@@ -1,3 +1,17 @@
+create table events (event_id SERIAL PRIMARY KEY,
+                     title varchar(255),
+                     starts timestamp,
+                     ends timestamp,
+                     venue_id integer,
+                     FOREIGN KEY (venue_id) references venues (venue_id));
+
+insert into events (title, starts, ends, venue_id)
+  values ('LARP Club', '2012-02-15 17:30:00', '2012-02-15 19:30:00', 2);
+insert into events (title, starts, ends)
+  values ('April Fools Day', '2012-04-01 00:00:00', '2012-04-01 23:59:00');
+insert into events (title, starts, ends)
+  values ('Christmas Day', '2012-12-25 00:00:00', '2012-12-25 23:59:00');
+
 -- Exercise 1
 
 select c.* from pg_class c, pg_namespace n
